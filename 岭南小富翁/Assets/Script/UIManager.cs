f8c866bbdc14bb306743522a200e94f3efb88d3f
Yljwv <3481845314@qq.com>
2026-06-05 17:05:09 +0800

    修复乱码

--- 岭南小富翁/Assets/Script/UIManager.cs
+++ 岭南小富翁/Assets/Script/UIManager.cs
@@ -65,7 +65,7 @@ public class UIManager : MonoBehaviour
     public Text persistentToastText;
     public Vector2 toastPosition = new Vector2(20, 20);
     
-    [Header("???????????UI")]
+    [Header("建筑放置提示UI")]
     public GameObject buildingPlacementHintPanel;
     public TextMeshProUGUI buildingPlacementHintText;
 
@@ -651,7 +651,7 @@ public class UIManager : MonoBehaviour
         }
 
         // 3. 
-        ShowPersistentToast($"??????: {building.buildingName}");
+        ShowPersistentToast($"当前选择: {building.buildingName}");
 
         // 4. 
         HighlightPlaceableTiles(currentBuildingPlayer, (int)building.requiredScale);
